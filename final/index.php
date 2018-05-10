12e67b626616ef34716fe2e105fbf2c4220ef992
Matthew P Molinari <mpm54@afsconnect1.njit.edu>
2018-05-10 13:02:08 -0400

    added form in index

---
+++ final/index.php
@@ -0,0 +1,39 @@
+<?php
+// Values
+$formEmail = filter_input(INPUT_POST, 'email');
+$formPass = filter_input(INPUT_POST, 'pass');
+?>
+<form class="form-horizontal" method = "post" action = "/todo_controller/index.php">
+    <fieldset>
+
+        <!-- Form Name -->
+        <h3>Login Todo</h3>
+
+        <!-- Email -->
+        <div class="form-group">
+            <label class="label" for="email">Email</label>
+            <div class="div">
+                <input id="email" name="email" type="email" class="emailinput">
+
+            </div>
+        </div>
+
+        <!-- Password -->
+        <div class="form-group">
+            <label class="label" for="pass">Password</label>
+            <div class="div">
+                <input id="pass" name="pass" type="password" class="passinput">
+
+            </div>
+        </div>
+
+        <!-- Submit -->
+        <div class="form-group">
+            <label class="label" for="submit"></label>
+            <div class="div">
+                <input name="submit" type="submit">
+            </div>
+        </div>
+
+    </fieldset>
+</form>
